9da9016fa06105e4072c1274e5f5c90e20980992
abivmsul <abdulmaliksultan15@gmail.com>
2026-05-13 17:18:33 +0300

    prisma.ts updated

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,21 +1,28 @@
-import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config"
+import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
-};
-
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) {
-  throw new Error("DATABASE_URL is missing");
+  prisma?: PrismaClient
 }
 
-const adapter = new PrismaPg({ connectionString });
+const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is missing")
+}
+
+const adapter = new PrismaPg({
+  connectionString,
+})
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["error"],
+  })
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prisma
 }

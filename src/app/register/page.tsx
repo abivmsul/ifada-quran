8184a47584eb7880c6f8cca675af96385ae88c71
abivmsul <abdulmaliksultan15@gmail.com>
2026-06-15 15:47:09 +0300

    student registration

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@ import StudentRegistrationForm from "@/components/student-registration-form"
 
 export default function RegisterPage() {
   return (
-    <div className="space-y-6 p-4 sm:p-6 lg:p-8">
+    <div className="space-y-6 p-4 sm:p-6 lg:p-8 bg-white  shadow">
       <StudentRegistrationForm mode="public" submitUrl="/api/register" />
     </div>
   )
